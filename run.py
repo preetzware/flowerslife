1ca4cb465838c3630bece18e471d82d7f6258970
Preety Ramdin <devianeeramdin@outlook.com>
2024-05-17 14:36:14 +0000

    Create welcome_message function and print ASCII banner and flower

--- run.py
+++ run.py
@@ -26,3 +26,52 @@ def clear_screen():
     clears console screen.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def welcome_message():
+    """
+    Displays the welcome message.
+    """
+    clear_screen()
+
+    print(r"""
+    
+       __       __)
+      (, ) |  /     /)
+        | /| /  _  // _  ______    _    _/_ ___
+        |/ |/ _(/_(/_(__(_) // (__(/_   (__(_)
+        /  |                                                         
+""")
+   
+
+    FLOWER = r""" 
+        _
+      _(_)_                          wWWWw   _
+     (_)@(_)   vVVVv     _     @@@@  (___) _(_)_
+       (_)\    (___)   _(_)_  @@()@@   Y  (_)@(_)
+        `  |/    Y    (_)@(_)  @@@@   \|/   (_)\
+          \|    \|/    /(_)    \|      |/      |
+           | / \ | /  \|/       |/    \|      \|/
+        \\\|//\\\|/// \|///  \\\|//  \\|//  \\\|// 
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """
+
+    BANNER = r"""
+        ________)                                          _            
+      (, /     /)                        /)           ___/__) ,   /)   
+        /___, // ____   _  _  __  _     // _____     (, /        //  _ 
+    ) /     (/_(_) (_(/ _(/_/ (_/_)_  /(_(_)/ (_      /    _(_ /(__(/_
+    (_/                               /)              (_____   /)      
+                                    (/                      )(/  
+    """
+
+    # Print FLOWER and BANNER
+    print(FLOWER)
+    print(BANNER)
+    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+    input("Press (Enter) to go to the main menu\n")
+
+
+
+if __name__ == '__main__':
+    welcome_message()
+    main_menu()
