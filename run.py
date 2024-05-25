46fd5cbb09e818e18d295d30c2f31687c31a5d38
Preety Ramdin <devianeeramdin@outlook.com>
2024-05-25 18:13:01 +0000

    Fix indentation issues

--- run.py
+++ run.py
@@ -40,14 +40,16 @@ def welcome_message():
     """
     Displays the welcome message only once at the start of the program.
     """
-    print(r"""
+    clear_screen()
+
+print(r"""
       __      __   _                    _
       \ \    / /__| |__ ___ _ __  ___  | |_ ___
        \ \/\/ / -_) / _/ _ \ '  \/ -_) |  _/ _ \
         \_/\_/\___|_\__\___/_|_|_\___|  \__\___/
     """)
     
-    FLOWER = r"""
+FLOWER = r"""
         _
       _(_)_                          wWWWw   _
      (_)@(_)   vVVVv     _     @@@@  (___) _(_)_
@@ -59,17 +61,17 @@ def welcome_message():
      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     """
     
-    BANNER = r"""
+BANNER = r"""
       ___ _                          __           _    _  __
      | __| |_____ __ _____ _ _ ___  / _|___ _ _  | |  (_)/ _|___
      | _|| / _ \ V  V / -_) '_(_-< |  _/ _ \ '_| | |__| |  _/ -_)
      |_| |_\___/\_/\_/\___|_| /__/ |_| \___/_|   |____|_|_| \___|
     """
 
-    print(f"{BRIGHT_GREEN}{FLOWER}{RESET}")
-    print(f"{BRIGHT_BLUE}{BANNER}{RESET}")
-    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-    input("Press (Enter) to go to the main menu\n")
+print(f"{BRIGHT_GREEN}{FLOWER}{RESET}")
+print(f"{BRIGHT_BLUE}{BANNER}{RESET}")
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+input("Press (Enter) to go to the main menu\n")
 
 
 def main_menu():
